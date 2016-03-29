b21cf1d240f5f6e99b9bf4a79c73dbf9a808f641
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-29 03:22:47 -0600

    Implemented symbol map

--- src/com/csci468/micro/Scope.java
+++ src/com/csci468/micro/Scope.java
@@ -1,10 +1,16 @@
 package com.csci468.micro;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Michael Shihrer
  * Matthew Johnerson
  * 26 March 2016
  */
 class Scope {
+    Map<String, Symbol> symbolMap = new HashMap<>();
+
+    //Add method to insert a symbol to the scope
 }
