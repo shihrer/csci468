e92b72b6bb2c89d016db8d26125febc998f184c7
MattJohnerson <mjohnerson@yahoo.com>
2016-03-29 12:38:48 -0600

    Finished listener functions

--- src/com/csci468/micro/Listener.java
+++ src/com/csci468/micro/Listener.java
@@ -10,6 +10,7 @@ import java.util.Stack;
  */
 
 class Listener extends MicroBaseListener {
+    private int count = 1;
 
     @Override
     public void enterProgram(MicroParser.ProgramContext ctx){
@@ -32,7 +33,8 @@ class Listener extends MicroBaseListener {
     @Override
     public void enterIfStmt(MicroParser.IfStmtContext ctx){
         //Create new scope
-        String name = ctx.getText();
+        String name = "BLOCK " + count;
+        count++;
         String output = String.format("Symbol table %s", name);
 
         System.out.println(output);
@@ -41,7 +43,8 @@ class Listener extends MicroBaseListener {
     @Override
     public void enterWhileStmt(MicroParser.WhileStmtContext ctx){
         //Create new scope
-        String name = ctx.getText();
+        String name = "BLOCK " + count;
+        count++;
         String output = String.format("Symbol table %s", name);
 
         System.out.println(output);
@@ -49,21 +52,52 @@ class Listener extends MicroBaseListener {
 
     @Override
     public void enterElsePart(MicroParser.ElsePartContext ctx){
-        String name = ctx.getText();
-        String output = String.format("Symbol table %s", name);
+        if(!ctx.getText().equals("")){
+            String name = "BLOCK " + count;
+            count++;
 
-        System.out.println(output);
+            String output = String.format("Symbol table %s", name);
+
+            System.out.println(output);
+        }
     }
 
     @Override
     public void enterStringDecl(MicroParser.StringDeclContext ctx){
         //Create an entry in the current scope
+        String input = ctx.getText();
+        int cut1 = input.indexOf(":=");
+        String name = input.substring(6,cut1);
+        String value = input.substring(cut1+2,input.length()-1);
+        String output = String.format("name %s type STRING value %s", name, value);
 
+        System.out.println(output);
     }
 
     @Override
     public void enterVarDecl(MicroParser.VarDeclContext ctx){
         //Create an entry in the current scope
+        String input = ctx.getText();
+        if(input.startsWith("FLOAT")){
+            String names = input.substring(5,input.length());
+            String[] tokens = names.split(",");
+            for (String s : tokens){
+                s = s.replace(";","");
+                String output = String.format("name %s type FLOAT", s);
+                System.out.println(output);
+            }
+        }
+        if(input.startsWith("INT")) {
+            String names = input.substring(3,input.length());
+            String[] tokens = names.split(",");
+            for (String s : tokens){
+                s = s.replace(";","");
+                String output = String.format("name %s type INT", s);
+                System.out.println(output);
+            }
+        }
+
+
 
     }
 }
