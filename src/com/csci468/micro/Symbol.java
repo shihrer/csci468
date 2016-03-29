c2f24c97bb1926272af0265454d834276034b244
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-29 12:35:29 -0600

    Symbol implementation

--- src/com/csci468/micro/Symbol.java
+++ src/com/csci468/micro/Symbol.java
@@ -6,6 +6,23 @@ package com.csci468.micro;
  * 28 March 2016
  */
 class Symbol {
-    protected Scope scope;
     protected String name;
+    protected String type;
+    protected String value;
+
+    public Symbol(String name, String type, String value){
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+
+    public Symbol(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public String toString(){
+        throw new UnsupportedOperationException("This is not implemented yet.");
+    }
 }
