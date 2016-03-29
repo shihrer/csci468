e56dce1b345a7f6d399b809c8e5c45410f657745
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-29 12:35:16 -0600

    Implemented create symbol method

--- src/com/csci468/micro/Scope.java
+++ src/com/csci468/micro/Scope.java
@@ -13,4 +13,9 @@ class Scope {
     Map<String, Symbol> symbolMap = new HashMap<>();
 
     //Add method to insert a symbol to the scope
+    public void createSymbol(){
+        //Create new symbol
+        //Add it to a scope
+        throw new UnsupportedOperationException("This is not implemented yet.");
+    }
 }
