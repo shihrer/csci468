3e63db7f62d5687e1ed7758e7712c1881f184fb3
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-29 03:24:13 -0600

    Initial creation of SymbolTable class.

---
+++ src/com/csci468/micro/SymbolTable.java
@@ -0,0 +1,20 @@
+package com.csci468.micro;
+
+import java.util.Stack;
+
+/**
+ * Michael Shihrer
+ * Matthew Johnerson
+ * 28 March 2016
+ */
+public class SymbolTable {
+    protected Stack<Scope> scopeStack;
+
+    public Scope pushScope(){
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public Scope popScope(){
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
