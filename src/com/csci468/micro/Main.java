69986b7bf41f5a8fcc81400d8c732ab86c4305ec
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-29 20:31:37 -0600

    Just a test string.

--- src/com/csci468/micro/Main.java
+++ src/com/csci468/micro/Main.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 class Main {
 
     public static void main(String[] args) throws IOException {
+        args = new String[1];
+        args[0] = "resources//Step3//inputs//test6.micro";
+
         if(args.length > 0) {
                 String input = args[0];
                 Micro micro = new Micro(input);
                 micro.Scan();
-
         }
         else
             System.out.println("You must supply an input.");
