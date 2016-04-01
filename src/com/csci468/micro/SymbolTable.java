d0cceca2cb64996bac6d8eacd569434c7979d748
Michael Shihrer <michael.shihrer@gmail.com>
2016-04-01 00:34:43 -0600

    Added some comments and refactored/renamed some functions.

--- src/com/csci468/micro/SymbolTable.java
+++ src/com/csci468/micro/SymbolTable.java
@@ -10,52 +10,60 @@ import java.util.Stack;
  */
 class SymbolTable {
     private Stack<Scope> scopeStack;
+
+    //Store all scopes.  Necessary for outputting.
     private ArrayList<Scope> scopes;
 
     SymbolTable(){
+        //Initialize stack
         scopeStack = new Stack<>();
         scopes = new ArrayList<>();
+
+        //Create the default, global scope.
         Scope global = new Scope("GLOBAL");
         scopeStack.push(global);
         scopes.add(global);
     }
 
-    Scope pushScope(String name){
+    Scope createScope(String name){
         Scope scope = new Scope(name);
         scopeStack.push(scope);
         scopes.add(scope);
 
         //System.out.println(String.format("Symbol table %s", name));
-
         return scope;
     }
 
-    Scope popScope(){
+    Scope destroyScope(){
         return scopeStack.pop();
     }
 
     //Define a symbol
-    void createSymbol(String name, String type, String value) {
+    Symbol createSymbol(String name, String type, String value) {
         Symbol symbol = new Symbol(name, type, value);
 
         scopeStack.peek().addSymbol(symbol);
         String output = String.format("name %s type %s value %s", name, type, value);
         //System.out.println(output);
+
+        return symbol;
     }
-    void createSymbol(String name, String type){
+    Symbol createSymbol(String name, String type){
         Symbol symbol = new Symbol(name, type);
 
         scopeStack.peek().addSymbol(symbol);
         String output = String.format("name %s type %s", name, type);
         //System.out.println(output);
+
+        return symbol;
     }
 
     @Override
     public String toString(){
         StringBuilder output = new StringBuilder();
-        for(Scope scope : scopes){
+        for(Scope scope : scopes)
             output.append(scope);
-        }
+
         return output.toString();
     }
 }
