53968b109e2c3449d38eec1d45a86514b6b7e32b
Matt <mjohnerson@yahoo.com>
2016-04-28 14:30:22 -0600

    created Tiny Generator class along with testing input IRNodes. This version produces correct output for test_adv.micro

--- src/com/csci468/micro/Micro.java
+++ src/com/csci468/micro/Micro.java
@@ -8,6 +8,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 class Micro {
     private MicroLexer lexer;
@@ -33,8 +34,67 @@ class Micro {
             ParseTree tree = parser.program();
             Listener microListener = new Listener();
             ParseTreeWalker.DEFAULT.walk(microListener, tree);
+            //Testing functionality
+            LinkedList<IRNode> list = new LinkedList<>();
+            list.add(new IRNode("VAR", "count", "", ""));
+            list.add(new IRNode("VAR", "enough", "", ""));
+            list.add(new IRNode("VAR", "newapprox", "", ""));
+            list.add(new IRNode("VAR", "approx", "", ""));
+            list.add(new IRNode("VAR", "num", "", ""));
+            list.add(new IRNode("VAR", "tolerance", "", ""));
+            list.add(new IRNode("VAR", "diff", "", ""));
+            list.add(new IRNode("LABEL", "main", "", ""));
+            list.add(new IRNode("LINK", "", "", ""));
+            list.add(new IRNode("STORER", "0.0001", "$T1", ""));
+            list.add(new IRNode("STORER", "$T1", "tolerance", ""));
+            list.add(new IRNode("STORER", "7.0", "$T2", ""));
+            list.add(new IRNode("STORER", "$T2", "num", ""));
+            list.add(new IRNode("STORER", "num", "approx", ""));
+            list.add(new IRNode("STOREI", "0", "$T3", ""));
+            list.add(new IRNode("STOREI", "$T3", "count", ""));
+            list.add(new IRNode("STORER", "0.0", "$T4", ""));
+            list.add(new IRNode("STORER", "$T4", "diff", ""));
+            list.add(new IRNode("STOREI", "0", "$T5", ""));
+            list.add(new IRNode("STOREI", "$T5", "enough", ""));
+            list.add(new IRNode("LABEL", "label1", "", ""));
+            list.add(new IRNode("STOREI", "1", "$T6", ""));
+            list.add(new IRNode("EQI", "enough", "$T6", "label2"));
+            list.add(new IRNode("STOREI", "1", "$T7", ""));
+            list.add(new IRNode("ADDI", "count", "$T7", "$T8"));
+            list.add(new IRNode("STOREI", "$T8", "count", ""));
+            list.add(new IRNode("STORER", "0.5", "$T9", ""));
+            list.add(new IRNode("DIVR", "num", "approx", "$T10"));
+            list.add(new IRNode("ADDR", "approx", "$T10", "$T11"));
+            list.add(new IRNode("MULR", "$T9", "$T11", "$T12"));
+            list.add(new IRNode("STORER", "$T12", "newapprox", ""));
+            list.add(new IRNode("SUBR", "approx", "newapprox", "$T13"));
+            list.add(new IRNode("STORER", "$T13", "diff", ""));
+            list.add(new IRNode("STORER", "0.0", "$T14", ""));
+            list.add(new IRNode("LER", "diff", "$T14", "label3"));
+            list.add(new IRNode("GER", "diff", "tolerance", "label4"));
+            list.add(new IRNode("STOREI", "1", "$T15", ""));
+            list.add(new IRNode("STOREI", "$T15", "enough", ""));
+            list.add(new IRNode("LABEL", "label4", "", ""));
+            list.add(new IRNode("JUMP", "label5", "", ""));
+            list.add(new IRNode("LABEL", "label3", "", ""));
+            list.add(new IRNode("STORER", "0.0", "$T16", ""));
+            list.add(new IRNode("SUBR", "$T16", "tolerance", "$T17"));
+            list.add(new IRNode("LER", "diff", "$T17", "label6"));
+            list.add(new IRNode("STOREI", "1", "$T18", ""));
+            list.add(new IRNode("STOREI", "$T18", "enough", ""));
+            list.add(new IRNode("LABEL", "label6", "", ""));
+            list.add(new IRNode("LABEL", "label5", "", ""));
+            list.add(new IRNode("STORER", "newapprox", "approx", ""));
+            list.add(new IRNode("JUMP", "label1", "", ""));
+            list.add(new IRNode("LABEL", "label2", "", ""));
+            list.add(new IRNode("WRITER", "approx", "", ""));
+            list.add(new IRNode("WRITEI", "count", "", ""));
+            list.add(new IRNode("RET", "", "", ""));
 
-            System.out.print(microListener);
+
+            TinyGenerator gen = new TinyGenerator(list);
+
+            //System.out.print(microListener);
 
         }catch(ParseCancellationException e){
             System.out.println(e.getMessage());
---
+++ src/com/csci468/micro/TinyGenerator.java
@@ -0,0 +1,145 @@
+package com.csci468.micro;
+
+import java.util.LinkedList;
+
+class TinyGenerator {
+
+    //Stores var declarations
+    private StringBuilder vars = new StringBuilder();
+    //Builds the Tiny code
+    private StringBuilder tiny = new StringBuilder();
+
+    private LinkedList<IRNode> code;
+
+    TinyGenerator (LinkedList<IRNode> IRcode){
+        code = IRcode;
+        generate();
+    }
+
+    private void generate(){
+        while (!code.isEmpty()){
+            IRNode current = code.pop();
+            int register = 0;
+            String opcode = current.get_opcode();
+            String f1 = current.get_first();
+            String f2 = current.get_second();
+            String f3 = current.get_third();
+            //Temp register for cases such as: a := b;
+            //need to first assign a to r99 and then r99 to b.
+            String temp = "r99";
+
+            switch (opcode){
+                //int and float variable declaration
+                case "VAR":
+                    vars.append(String.format("var %s\n", f1));
+                    break;
+                //String variable declaration
+                case "STR":
+                    vars.append(String.format("var %s %s\n", f1, f2));
+                    break;
+                //Labels
+                case "LABEL":
+                    tiny.append(String.format("label %s\n", f1));
+                    break;
+                //Unconditional jump
+                case "JUMP":
+                    tiny.append(String.format("jmp %s\n", f1));
+                    break;
+                //Read inputs
+                case "READI":
+                case "READR":
+                    tiny.append(String.format("sys %s %s\n", opcode.toLowerCase(), f1));
+                    break;
+                //Assignment statements
+                case "STOREI":
+                case "STORER":
+                    //check to catch statements such as a := b;
+                    if(!f1.contains("$") && !f2.contains("$")){
+                        tiny.append(String.format("move %s %s\n", f1.replace("$T", "r"), temp));
+                        tiny.append(String.format("move %s %s\n", temp, f2.replace("$T", "r")));
+                    }else {
+                        tiny.append(String.format("move %s %s\n", f1.replace("$T", "r"), f2.replace("$T", "r")));
+                    }
+                    break;
+                //Increment and decriment
+                case "INCI":
+                case "DECI":
+                    tiny.append(String.format("%s %s\n", opcode.toLowerCase(), f1.replace("$T","r")));
+                    break;
+                //Addition
+                case "ADDI":
+                case "ADDR":
+                    tiny.append(String.format("move %s %s\n", f1.replace("$T","r"), f3.replace("$T","r")));
+                    tiny.append(String.format("%s %s %s\n", opcode.toLowerCase(), f2.replace("$T","r"), f3.replace("$T","r")));
+                    break;
+                //Subtraction
+                case "SUBI":
+                case "SUBR":
+                    tiny.append(String.format("move %s %s\n", f1.replace("$T","r"), f3.replace("$T","r")));
+                    tiny.append(String.format("%s %s %s\n", opcode.toLowerCase(), f2.replace("$T","r"), f3.replace("$T","r")));
+                    break;
+                //Multiplication
+                case "MULI":
+                case "MULR":
+                    tiny.append(String.format("move %s %s\n", f1.replace("$T","r"), f3.replace("$T","r")));
+                    tiny.append(String.format("%s %s %s\n", opcode.toLowerCase(), f2.replace("$T","r"), f3.replace("$T","r")));
+                    break;
+                //Division
+                case "DIVI":
+                case "DIVR":
+                    tiny.append(String.format("move %s %s\n", f1.replace("$T","r"), f3.replace("$T","r")));
+                    tiny.append(String.format("%s %s %s\n", opcode.toLowerCase(), f2.replace("$T","r"), f3.replace("$T","r")));
+                    break;
+
+                //Integer comparison
+                case "EQI":
+                case "NEI":
+                case "GEI":
+                case "LEI":
+                case "LTI":
+                case "GTI":
+                    if(!f1.contains("$") && !f2.contains("$")){
+                        tiny.append(String.format("move %s %s\n", f2, temp));
+                        tiny.append(String.format("cmpi %s %s\n", f1, temp));
+                    }else {
+                        tiny.append(String.format("cmpi %s %s\n", f1, f2.replace("$T","r")));
+                    }
+                    tiny.append(String.format("j%s %s\n", opcode.substring(0,2).toLowerCase(), f3));
+                    break;
+                //Real number comparison
+                case "EQR":
+                case "NER":
+                case "GER":
+                case "LER":
+                case "LTR":
+                case "GTR":
+                    if(!f1.contains("$") && !f2.contains("$")){
+                        tiny.append(String.format("move %s %s\n", f2, temp));
+                        tiny.append(String.format("cmpr %s %s\n", f1, temp));
+                    }else {
+                        tiny.append(String.format("cmpr %s %s\n", f1, f2.replace("$T","r")));
+                    }
+                    tiny.append(String.format("j%s %s\n", opcode.substring(0,2).toLowerCase(), f3));
+                    break;
+                //System output
+                case "WRITEI":
+                case "WRITES":
+                case "WRITER":
+                    tiny.append(String.format("sys %s %s\n", opcode.toLowerCase(), f1));
+                    break;
+                //Links for function calls
+                case "LINK":
+                case "RET":
+                    //unused
+                    break;
+                default:
+                    System.out.println("Invalid opcode: " + opcode);
+
+            }
+        }
+        tiny.append("sys halt\n");
+        tiny.insert(0, vars);
+    }
+
+    public StringBuilder getTiny(){ return tiny; }
+}
